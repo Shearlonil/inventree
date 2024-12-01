45e15879d89888a6c336847d03f709c4018d8efe
joshuaodunbaku <joshuaodunbaku@gmail.com>
2024-12-02 00:47:16 +0100

    working on home page

--- src/Routes/Home.jsx
+++ src/Routes/Home.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import SVG from "../Assets/Svg";
 import { HomeWrapper } from "../Components/Styles/HomeStyles";
 
+import { IoIosRefresh } from "react-icons/io";
+
 const Home = () => {
 	return (
 		<HomeWrapper>
@@ -35,7 +37,7 @@ const Home = () => {
 			<div className="container" id="section-2">
 				<div className="row">
 					<div className="col-12 col-md-8 p-2" id="child-1">
-						<div className="p-3 h-100 py-auto bg-primary rounded d-flex flex-column justify-content-center">
+						<div className="p-3 h-100 py-auto bg-primary rounded d-flex flex-column justify-content-around">
 							<h2 className="fw-bold">Client Store Name</h2>
 							<p>App Mode: Single/Multi Tier</p>
 							<div>
@@ -45,11 +47,15 @@ const Home = () => {
 					</div>
 
 					<div className="col-12 col-md-4 p-2" id="child-2">
-						<div className="p-3 h-100 py-auto bg-warning rounded">
-							<h2 className="fw-bold slide-top">Incomplete Transactions</h2>
-							<div className="d-flex">
-								<span>"3"</span>
-								<span>Refresh Icon</span>
+						<div className="p-3 h-100 py-auto bg-warning rounded d-flex flex-column justify-content-around">
+							<h2 className="fw-bold">Incomplete Transactions</h2>
+							<div>
+								<div className="d-flex justify-content-between">
+									<span className="space-mono-bold h2">"3"</span>
+									<span>
+										<IoIosRefresh size={"40"} />
+									</span>
+								</div>
 							</div>
 							<p>Invoices with receipts</p>
 						</div>
