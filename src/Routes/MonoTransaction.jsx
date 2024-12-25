bce708326144cddb48f817667e50b6269c1c0176
joshuaodunbaku <joshuaodunbaku@gmail.com>
2024-12-25 04:03:42 +0100

    updated the Mono Transaction Page

--- src/Routes/MonoTransaction.jsx
+++ src/Routes/MonoTransaction.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Form, Table } from "react-bootstrap";
+import { BiMinus, BiPlus } from "react-icons/bi";
 import { CgAdd } from "react-icons/cg";
+import { HiMiniWallet } from "react-icons/hi2";
 import { IoAddSharp } from "react-icons/io5";
 import Select from "react-select";
 
@@ -138,6 +140,235 @@ const MonoTransaction = () => {
 							))}
 						</tbody>
 					</Table>
+				</div>
+			</div>
+			{/*  */}
+			<div className="container my-4 text-center">
+				<h2 className="mb-2">
+					Total (N): <span className="text-danger">9560.00</span>
+				</h2>
+				<div className="d-flex justify-content-center align-items-center gap-4">
+					<input
+						type="number"
+						className="form-control"
+						id="discount_value"
+						placeholder="0"
+						style={{ width: "160px" }}
+					/>
+
+					<Form.Check type="radio" label="N" value="unit" name="invoice_disc" />
+					<Form.Check type="radio" label="%" value="perc" name="invoice_disc" />
+					<div className="btn btn-success fw-bold p-2 px-2 d-flex align-items-center justify-content-center">
+						<BiPlus />
+					</div>
+					<div className="btn btn-danger fw-bold p-2 px-2 d-flex align-items-center justify-content-center">
+						<BiMinus />
+					</div>
+				</div>
+			</div>
+			{/*  */}
+			<div className="container">
+				<div className="border bg-secondary-subtle p-3 rounded-4">
+					<div className="row">
+						<div className="col-7 border border-2 border-primary rounded-2">
+							<h3 className="text-primary mt-3">Customer Details</h3>
+							<div
+								className="d-flex flex-column gap-4"
+								style={{ minHeight: "300px" }}
+							>
+								<div className="row mb-2 align-items-center">
+									<div className="col-12 col-sm-4">
+										<p className="text-sm-end h5">Search By:</p>
+									</div>
+									<div className="col-12 col-sm-8">
+										<Form.Group className="my-2">
+											<div className="pe-3">
+												<div className="d-flex gap-5">
+													<Form.Check
+														type="radio"
+														label="Name"
+														value="name"
+														name="search_param"
+													/>
+													<Form.Check
+														type="radio"
+														label="Card NO."
+														value="card_no"
+														name="search_param"
+													/>
+												</div>
+											</div>
+										</Form.Group>
+									</div>
+								</div>
+
+								<div className="row mb-2">
+									<div className="col-4">
+										<p className="text-sm-end h5">Full Name:</p>
+									</div>
+									<div className="col-8">
+										<p className="text-success fw-bold">Customer</p>
+									</div>
+								</div>
+
+								<div className="row mb-2">
+									<div className="col-4">
+										<p className=" text-end">
+											<HiMiniWallet size={25} />
+										</p>
+									</div>
+									<div className="col-8 d-flex flex-column gap-3">
+										<div className="d-flex gap-5">
+											<p className="text-danger">1100.00</p>
+											<p>
+												Disount {"(%)"}: <span className="text-success">0</span>
+											</p>
+										</div>
+										<label
+											className="d-flex gap-2 text-success fw-bold"
+											htmlFor="deduct_wallet_discount_from_payment"
+										>
+											<Form.Check
+												type="checkbox"
+												value="yes"
+												className="shadow-sm p-0 m-0"
+												id="deduct_wallet_discount_from_payment"
+												name="deduct_wallet_discount_from_payment"
+											/>
+											deduct wallet discount from payment
+										</label>
+									</div>
+								</div>
+							</div>
+						</div>
+
+						{/*  */}
+						<div className="col-5">
+							<div className="row mb-2">
+								<div className="col-12 col-sm-4">
+									<p className="text-sm-end h5">Payment Mode:</p>
+								</div>
+								<div className="col-12 col-sm-8">
+									<div className="d-flex gap-3 flex-wrap mt-2">
+										<div className="d-flex flex-column gap-3">
+											<label
+												className="d-flex gap-2 fw-bold"
+												htmlFor="payment_mode_cash"
+											>
+												<Form.Check
+													type="checkbox"
+													value="yes"
+													className="shadow-sm p-0 m-0"
+													id="payment_mode_cash"
+													name="payment_mode_cash"
+												/>
+												Cash
+											</label>
+											<input
+												type="number"
+												className="form-control"
+												id="payment_mode_cash_amount"
+												placeholder="0"
+											/>
+										</div>
+										<div className="d-flex flex-column gap-3">
+											<label
+												className="d-flex gap-2 fw-bold"
+												htmlFor="payment_mode_transfer"
+											>
+												<Form.Check
+													type="checkbox"
+													value="yes"
+													className="shadow-sm p-0 m-0"
+													id="payment_mode_transfer"
+													name="payment_mode_transfer"
+												/>
+												Transfer
+											</label>
+											<input
+												type="number"
+												className="form-control"
+												id="payment_mode_transfer_amount"
+												placeholder="0"
+											/>
+										</div>
+										<div className="d-flex flex-column gap-3">
+											<label
+												className="d-flex gap-2 fw-bold"
+												htmlFor="payment_mode_pos_atm"
+											>
+												<Form.Check
+													type="checkbox"
+													value="yes"
+													className="shadow-sm p-0 m-0"
+													id="payment_mode_pos_atm"
+													name="payment_mode_pos_atm"
+												/>
+												POS/ATM
+											</label>
+											<input
+												type="number"
+												className="form-control"
+												id="payment_mode_pos_atm_amount"
+												placeholder="0"
+											/>
+										</div>
+
+										<div className="d-flex flex-column gap-3">
+											<label
+												className="d-flex gap-2 fw-bold"
+												htmlFor="payment_mode_wallet"
+											>
+												<Form.Check
+													type="checkbox"
+													value="yes"
+													className="shadow-sm p-0 m-0"
+													id="payment_mode_wallet"
+													name="payment_mode_wallet"
+												/>
+												Wallet
+											</label>
+											<input
+												type="number"
+												className="form-control"
+												id="payment_mode_wallet_amount"
+												placeholder="0"
+											/>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						{/*  */}
+					</div>
+					<div className="d-flex justify-content-between align-items-center p-3">
+						<div>
+							<label className="d-flex gap-2" htmlFor="print_receipt">
+								<Form.Check
+									type="checkbox"
+									value="yes"
+									className="shadow-sm p-0 m-0"
+									id="print_receipt"
+									name="print_receipt"
+								/>
+								Print Receipt
+							</label>
+						</div>
+						<div className="d-flex gap-3">
+							<button
+								className="btn btn-danger rounded-pill"
+								style={{ width: "160px" }}
+							>
+								Cancel
+							</button>
+							<button
+								className="btn btn-success rounded-pill"
+								style={{ width: "160px" }}
+							>
+								OK
+							</button>
+						</div>
+					</div>
 				</div>
 			</div>
 		</>
